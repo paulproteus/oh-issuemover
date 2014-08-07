1a4217782163fa48c66bd34e18c5d9902b0f2b58
Asheesh Laroia <asheesh@asheesh.org>
2014-08-06 19:04:52 -0700

    wip

--- upload.py
+++ upload.py
@@ -1,10 +1,14 @@
-oauth_token="773ab19df1116df1623d5effd07fcab6571b42c8"
+oauth_token="74aba8524476083e805734e59784693fffdc960a"
 
 import urllib2, json
+import glob
 
-c=1
-while c != 949:
-	print "Adding bug "+str(c)+"/949"
+bug_files = glob.glob('bugs/*')
+without_dir_name = [x.replace('bugs/', '') for x in bug_files]
+as_int = sorted([int(x) for x in without_dir_name])
+
+for c in as_int:
+	print "Adding bug "+str(c)+"/" + str(len(as_int))
 	bug=json.load(open("bugs/"+str(c)))
 	openClosed='open'
 	body=""
@@ -40,7 +44,7 @@ while c != 949:
 	body+="Waiting On : <b>"+bug["waitingon"]+"</b>\n"
 	body+="Roundup ID : <b>"+bug["id"]+"</b>\n"
 
-	req = urllib2.Request('https://api.github.com/repos/602p/oh-issues/issues?access_token='+oauth_token,
+	req = urllib2.Request('https://api.github.com/repos/paulproteus/test-bug-import/issues?access_token='+oauth_token,
 			json.dumps({
 				'title':bug["title"],
 				'body':body,
@@ -49,7 +53,7 @@ while c != 949:
 			})
 		)
 	resp = json.load(urllib2.urlopen(req))
-	req = urllib2.Request('https://api.github.com/repos/602p/oh-issues/issues/'+str(resp["number"])+'?access_token='+oauth_token,
+	req = urllib2.Request('https://api.github.com/repos/paulproteus/test-bug-import/issues/'+str(resp["number"])+'?access_token='+oauth_token,
 			json.dumps({
 				'state':openClosed
 			})
